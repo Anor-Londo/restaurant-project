ed55c739ddd8319dd3901cbca0f3ae74b469c6ea
Michael <Mishareznik@yandex.ru>
2019-02-18 11:33:12 +0300

    wiring up old test project

---
+++ ppmtool/src/main/java/com/bubbleinterface/ppmtool/repository/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.bubbleinterface.ppmtool.repository;
+
+import com.bubbleinterface.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    Project findByProjectIdentifier(String projectId);
+
+    @Override
+    Iterable<Project> findAll();
+}
