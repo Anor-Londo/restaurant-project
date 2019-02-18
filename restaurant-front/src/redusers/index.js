8da9d150369028738b744e1ca1c7f189095e1565
Michael <Mishareznik@yandex.ru>
2019-02-19 02:18:46 +0300

    working with jwtToken on the front-side + login

--- restaurant-front/src/redusers/index.js
+++ restaurant-front/src/redusers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import errorReduser from "./errorReduser";
 import guestReducer from "./guestReducer";
 import tableReducer from "./tableReducer";
+import securityReducer from "./securityReduser";
 
 export default combineReducers({
   errors: errorReduser,
   guest: guestReducer,
-  table: tableReducer
+  table: tableReducer,
+  security: securityReducer
 });
